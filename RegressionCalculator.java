6eb517ac68d4a7c3523ebdfad7d0e6261f32a32e
Taylor Sweetman <taylor.sweetman@gmail.com>
2018-01-31 01:52:10 -0500

    initial skeleton and classes

---
+++ RegressionCalculator.java
@@ -0,0 +1,32 @@
+package regressioncalculator;
+
+import java.util.Scanner;
+
+public class RegressionCalculator {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        Storage DB = new Storage();
+        int n = 0;
+
+        System.out.println("Enter x - y value pairs. When finished, enter a instead of a number.");
+        
+        while (true) {
+            System.out.println("Enter x" + n);
+            String input1 = reader.nextLine();
+            if (input1.equals("a")) {
+                break;
+            }
+            System.out.println("Enter y" + n);
+            String input2 = reader.nextLine();
+            if (input2.equals("a")) {
+                break;
+            }
+            Pair XYpair = new Pair(input1, input2);
+            DB.add(XYpair);
+            n++;
+        }
+        
+        System.out.println(DB);
+    }
+}
