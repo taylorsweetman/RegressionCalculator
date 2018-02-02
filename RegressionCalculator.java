db6177e9454b45225150bfc8da0512ce18ded12b
Taylor Sweetman <taylor.sweetman@gmail.com>
2018-02-01 21:14:38 -0500

    improved main class by functionizing some repeated code

--- RegressionCalculator.java
+++ RegressionCalculator.java
@@ -5,14 +5,24 @@ import java.util.Scanner;
 public class RegressionCalculator {
 
     public static void main(String[] args) {
+        /*fixedInput(1, 1);
+        fixedInput(2, 5);
+        fixedInput(3, 6);
+        fixedInput(4, 8);
+        fixedInput(5, 15);
+        fixedInput(6, 16);*/
+        inputProcess();
+    }
+
+    //creates a new x-y pair, adds it to storage, and increments sample size
+    public static void inputProcess() {
         Storage myData = new Storage();
-        Functions myFunctions = new Functions();
+        System.out.println("Enter x - y value pairs. When finished, enter \"a\" instead of a number.");
         Scanner reader = new Scanner(System.in);
         int n = 0;
 
-        // System.out.println("Enter x - y value pairs. When finished, enter \"a\" instead of a number.");
         //asks user for input pairs until "a" is entered
-        /*  while (true) {
+        while (true) {
             System.out.println("Enter x" + n);
             String input1 = reader.nextLine();
             if (input1.equals("a")) {
@@ -22,25 +32,18 @@ public class RegressionCalculator {
             String input2 = reader.nextLine();
             if (input2.equals("a")) {
                 break;
-            } */
-        //creates a new x-y pair, adds it to storage, and increments sample size
-        Pair xyPair = new Pair(1, 1);
-        myData.add(xyPair);
-        xyPair = new Pair(2, 5);
-        myData.add(xyPair);
-        xyPair = new Pair(3, 6);
-        myData.add(xyPair);
-        xyPair = new Pair(4, 8);
-        myData.add(xyPair);
-        xyPair = new Pair(5, 15);
-        myData.add(xyPair);
-        xyPair = new Pair(6, 16);
-        myData.add(xyPair);
-        n++;
-        myData.add(6);
+            }
 
+            Pair inputPair = new Pair(Integer.parseInt(input1), Integer.parseInt(input2));
+            myData.add(inputPair);
+            n++;
+        }
         System.out.println(myData);
     }
 
+    public static void fixedInput(int x, int y) {
+        Storage myData = new Storage();
+        Pair inputPair = new Pair(x, y);
+        myData.add(inputPair);
+    }
 }
-
--- Storage.java
+++ Storage.java
@@ -4,21 +4,18 @@ import java.util.ArrayList;
 
 public class Storage {
 
-    private ArrayList<Pair> table;
+    private final ArrayList<Pair> table;
     private int n;
-    private MyMath myMath;
+    private final MyMath myMath;
 
     public Storage() {
         table = new ArrayList<>();
         myMath = new MyMath(this);
+        n = 0;
     }
 
     public void add(Pair input) {
         table.add(input);
-    }
-
-    public void add(int n) {
-        this.n = n;
     }
 
     public ArrayList<Pair> getTable() {
@@ -26,16 +23,16 @@ public class Storage {
     }
 
     public int getN() {
+        n = table.size();
         return n;
     }
     
     @Override
     public String toString() {
         String str = "";
-        int n = 0;
         for (Pair entry : table) {
-            str += "x" + n + "--" + entry.getX() + "\ny" + n + "--" + entry.getY() + "\n";
-            n++;
+            str += "x" + getN() + "--" + entry.getX() + "\ny" + getN() + "--" + entry.getY() + "\n";
+            getN();
         }
         str += "xBar: " + myMath.xBar() + " yBar: " + myMath.yBar() + " xVar: " + myMath.xVar() + " yVar: " + myMath.yVar() + " xyCov: " + myMath.xyCov() + "\n";
         str += "y = " + myMath.beta0() + " + " + myMath.beta1() + " * x\n";
