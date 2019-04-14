209b17bceb58991b22eaf7dec9148653d8d09443
taylo <taylo@192.168.2.12>
2019-04-13 21:17:34 -0400

    fixed the entity error, but broke "Pair" and need to refactor test folde

--- src/calculator/domain/Pair.java
+++ src/calculator/domain/Pair.java
@@ -1,8 +1,10 @@
 package calculator.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity(name = "input_table")
 public class Pair {
 
     private final double X;
@@ -10,11 +12,7 @@ public class Pair {
     private final int index;
 
     //this constructor is used to create x, y input pairs when reading input from the user
-    public Pair(double x, double y, int index) {
-        this.X = x;
-        this.Y = y;
-        this.index = index;
-    }
+   
 
     @Column(name = "id")
 	@Id
@@ -22,11 +20,17 @@ public class Pair {
     	return index;
     }
 
+    @Column(name = "x_value")
     public double getX() {
         return X;
     }
 
+    @Column(name = "y_value")
     public double getY() {
         return Y;
+    }
+    
+    public void setX(double x) {
+    	this.X = x;
     }
 }
--- src/calculator/domain/PairTest.java
+++ src/calculator/domain/PairTest.java
@@ -1,25 +1,26 @@
+package calculator.domain;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-import calculator.domain.*;
+//import javax.persistence.Entity;
 
 public class PairTest {
 
 	public static void main(String[] args) {
 		Pair pair1 = new Pair(1, 1, 1);
-		
+
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Regression Calculator");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 
 		entityManager.getTransaction().begin();
-		
+
 		entityManager.persist(pair1);
-		
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		entityManagerFactory.close();
-		
+
 		System.out.println("A Users Object was persisted");
 	}
 }
